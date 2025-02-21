e0fd7aa95661590fa95cd7f7fea47977605b15f6
fasid123 <149943591+fasid123@users.noreply.github.com>
2025-02-22 01:59:37 +0530

    Create find Last Digit of Number.java

---
+++ Mathematical Concept/find Last Digit of Number.java
@@ -0,0 +1,13 @@
+class Main {
+    public static void main(String[] args) {
+        int num = 105654;
+        System.out.println(num%10);
+    }
+}
+
+otput is 4
+
+
+a number % 10 : 5462 modulo 10 answer is 2 that is the last number of 5462
+
+n % 10 - formula of finding last number of a digits - imp topic
