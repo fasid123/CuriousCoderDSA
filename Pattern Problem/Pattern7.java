d6a12a76ac89442fca31ee08f8c646ec2c6b671b
fasid123 <149943591+fasid123@users.noreply.github.com>
2025-02-22 00:25:37 +0530

    Create Pattern7.java

---
+++ Pattern Problem/Pattern7.java
@@ -0,0 +1,14 @@
+class Main {
+    public static void main(String[] args) {
+        
+        int n = 4;
+        for (int i = n; i > 0; i--){
+            for (int j = i; j >= 1 ;j--){
+                System.out.print(j);
+            }
+            System.out.println("");
+           
+        }
+        
+    }
+}
