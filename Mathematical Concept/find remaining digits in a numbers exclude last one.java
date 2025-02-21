ba431d7c5166c489a43df1ac97068309b5b014f4
fasid123 <149943591+fasid123@users.noreply.github.com>
2025-02-22 02:04:50 +0530

    Create find remaining digits in a numbers exclude last one.java

---
+++ Mathematical Concept/find remaining digits in a numbers exclude last one.java
@@ -0,0 +1,13 @@
+
+
+class Main {
+    public static void main(String[] args) {
+        int num = 105654;
+        System.out.println(num/10);
+    }
+}
+
+
+a number / 10 : 5462 divide 10 answer is 546 that is the first 3 number and exclude the last number of 5462
+
+n / 10 - formula of finding first series of number of a digits exclude the last digit.
