97f6abc52005b8735efb5e4253ac0d2c15308d11
fasid123 <149943591+fasid123@users.noreply.github.com>
2025-02-22 02:03:13 +0530

    Update find Last Digit of Number and find remaining number exclude the last digit.java

--- Mathematical Concept/find Last Digit of Number.java
+++ Mathematical Concept/find Last Digit of Number.java
@@ -11,3 +11,18 @@ otput is 4
 a number % 10 : 5462 modulo 10 answer is 2 that is the last number of 5462
 
 n % 10 - formula of finding last number of a digits - imp topic
+
+
+    ------------------------------------------------------
+
+class Main {
+    public static void main(String[] args) {
+        int num = 105654;
+        System.out.println(num/10);
+    }
+}
+
+
+a number / 10 : 5462 divide 10 answer is 546 that is the first 3 number and exclude the last number of 5462
+
+n / 10 - formula of finding first series of number of a digits exclude the last digit.
