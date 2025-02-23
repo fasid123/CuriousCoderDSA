2bd598e1c020eed7dd66c999e8b44f7bcf331bcf
fasid123 <149943591+fasid123@users.noreply.github.com>
2025-02-23 09:27:19 +0530

    Update Pattern9.java

--- Pattern Problem/Pattern9.java
+++ Pattern Problem/Pattern9.java
@@ -1,36 +1,4 @@
-Nandy code : 
-    
-class Main {
-    public static void main(String[] args) {
-        int num = 4;
-        for (int i = 1 ; i<= num;i++){
-            // space
-            for (int k = 1; k <= num-i ; k++){
-                System.out.print("  ");
-            }
-            
-            // stars
-            
-             for (int j = 1 ; j <= 2*i-1 ; j++){
-                System.out.print("* ");
-            }
-           
-            System.out.println("");
-        }
-    }
-}
 
-
-output:
-
-      * 
-    * * * 
-  * * * * * 
-* * * * * * * 
-
-
-
-my code:
 
 class Main {
     public static void main(String[] args) {
