1f9fe5ed69b83bb2a0fe8595e4ad9ccfc57671f2
fasid123 <149943591+fasid123@users.noreply.github.com>
2025-02-27 19:53:11 +0530

    Create find square root of N.java

---
+++ Mathematical Concept/find square root of N.java
@@ -0,0 +1,14 @@
+class Main {
+    public static void main(String[] args) {
+      int n = 25;
+      int square=0;
+      for (int i = 1; i * i <= n;i++){
+          square = i;
+          
+      }
+      System.out.println(square);
+    }
+}
+
+ouput:
+5
