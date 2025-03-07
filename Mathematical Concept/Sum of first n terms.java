194aa6ca68001fa7d29c52355db1b23a1d51131d
fasid123 <149943591+fasid123@users.noreply.github.com>
2025-03-07 16:19:32 +0530

    Update Sum of first n terms.java

--- Mathematical Concept/Sum of first n terms.java
+++ Mathematical Concept/Sum of first n terms.java
@@ -1,14 +1,16 @@
-Given an integer n, calculate the sum of series 13 + 23 + 33 + 43 + … till n-th term.
+Sum of n terms:
+
+Given an integer n, calculate the sum of series 1^3 + 2^3 + 3^3 + 4^3 + … till n-th term.
 
 Examples:
 
 Input: n = 5
 Output: 225
-Explanation: 13 + 23 + 33 + 43 + 53 = 225
+Explanation: 1^3 + 2^3 + 3^3 + 4^3 + 5^3 = 225
   
 Input: n = 7
 Output: 784
-Explanation: 13 + 23 + 33 + 43 + 53 + 63 + 73 = 784
+Explanation: 1^3 + 2^3 + 3^3 + 4^3 + 5^3 + 6^3 + 7^3 = 784
 
   code:
 import java.util.*;
@@ -18,19 +20,12 @@ class Main {
         
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
-        // code here
-     
-        // code here
         int sum = 0;
         for (int i = 1; i <= n; i++){
             sum = sum + i*i*i;
         }
       System.out.println(sum);
-
-
-    
 };
-
     }
 
 output:
