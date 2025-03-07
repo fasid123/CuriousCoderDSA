5d44af5203df99c5d76b47abcff8903512241e8c
fasid123 <149943591+fasid123@users.noreply.github.com>
2025-03-07 12:37:24 +0530

    Create Swap two variables values using temp.java

---
+++ Mathematical Concept/Swap two variables values using temp.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        
+       
+        int a = 2;
+        int b = 4;
+        int temp = a;
+        a = b;
+        b = temp;
+        
+       
+        System.out.println(a);
+          System.out.println(b);
+    
+
+    
+};
+
+    }
+output:
+4
+2
