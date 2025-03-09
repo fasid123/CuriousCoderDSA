3da41538e6b0938631ecc6d2bf8fd1e4828eef15
fasid123 <149943591+fasid123@users.noreply.github.com>
2025-03-09 13:27:29 +0530

    Create find array length.java

---
+++ DSA/array/find array length.java
@@ -0,0 +1,11 @@
+Find the array length using length keyword
+
+class Main {
+    public static void main(String[] args) {
+        int [] arr = new int[5];
+        System.out.println(arr.length);
+
+
+    }
+}
+Output : 5
