c5d7573cf3b4cfee3bba724039bfdfb2a15116dd
fasid123 <149943591+fasid123@users.noreply.github.com>
2025-03-09 13:23:54 +0530

    Create Create Array.java

---
+++ DSA/array/Create Array.java
@@ -0,0 +1,13 @@
+class Main {
+    public static void main(String[] args) {
+        int [] arr = new int[5];
+        arr[0] = 25;
+        arr[1] = 30;
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+    }
+}
+
+Output 
+25
+30
