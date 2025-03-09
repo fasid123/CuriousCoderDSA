d103a1b8bb25d53cf0d183357ed52e617adceed2
fasid123 <149943591+fasid123@users.noreply.github.com>
2025-03-09 13:24:44 +0530

    Create direct assign to array.java

---
+++ DSA/array/direct assign to array.java
@@ -0,0 +1,13 @@
+class Main {
+    public static void main(String[] args) {
+        
+        
+      //  int [] arr = {1,2,3,4,5,6};  : directly declare
+        
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+    }
+}
+
+Output : 1
+         2
