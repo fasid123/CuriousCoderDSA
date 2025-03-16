58fb5f878c19f81a5488275db609ae3458dcedd7
fasid123 <149943591+fasid123@users.noreply.github.com>
2025-03-16 20:11:04 +0530

    Update Insertion Sort.java

--- DSA/Sorting Algorithm/Insertion Sort.java
+++ DSA/Sorting Algorithm/Insertion Sort.java
@@ -1,1 +1,28 @@
 
+import java.util.*;
+
+// insertion sort 
+
+class Main {
+    public static void main(String[] args) {
+        int [] a = {1,3,6,2,4,0};
+        int n = 6;
+        
+        for (int i = 1; i< n; i++){
+            int key = a[i];
+            int j = i-1;
+            while( j >= 0 && key < a[j]){
+                a[j+1] = a[j];
+                j--;
+            }
+            a[j+1] = key;
+            
+        }
+        
+        System.out.println(Arrays.toString(a));
+        
+    }
+}
+
+output: 
+[0, 1, 2, 3, 4, 6]
